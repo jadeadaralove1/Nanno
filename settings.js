ec1842447b6fc8da7016d5a7cff1a77af91b3363
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-02 11:08:25 -0300

    Crear settings.js

---
+++ settings.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import { watchFile, unwatchFile } from 'fs'
+import { fileURLToPath } from 'url'
+
+global.owner = ['5493863402551', '5493863447787']
+global.botNumber = ''
+
+global.sessionName = 'Sessions/Owner'
+global.version = '^2.0 - Latest'
+global.dev = "Love With By Adara"
+global.links = {
+api: 'https://api.stellarwa.xyz',
+channel: "https://whatsapp.com/channel/0029VbBvrmwC1Fu5SYpbBE2A",
+github: "https://github.com/jadeadaralove1/Nanno-bot",
+}
+global.my = {
+ch: '120363406529946290@newsletter',
+name: 'NANNOBOT',
+}
+
+global.mess = {
+socket: '𐄹 ۪ ׁ ⚠️ᩚ̼ 𖹭̫ ▎ Este comando solo puede ser ejecutado por un Socket.',
+admin: '𐄹 ۪ ׁ ⚠️ᩚ̼ 𖹭̫ ▎ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+botAdmin: '𐄹 ۪ ׁ ⚠️ᩚ̼ 𖹭̫ ▎ Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+}
+
+global.APIs = {
+adonix: { url: "https://api-adonix.ultraplus.click", key: "Yuki-WaBot" },
+vreden: { url: "https://api.vreden.web.id", key: null },
+nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+stellar: { url: "https://api.stellarwa.xyz", key: "YukiWaBot" },
+apifaa: { url: "https://api-faa.my.id", key: null },
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null }
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  import(`${file}?update=${Date.now()}`)
+})

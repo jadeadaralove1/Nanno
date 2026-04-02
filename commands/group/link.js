067c73b95a661a23af1e3e892da517e474d51e46
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-02 16:14:23 -0300

    Actualizar link.js

--- commands/group/link.js
+++ commands/group/link.js
@@ -6,7 +6,13 @@ export default {
     try {
       const code = await client.groupInviteCode(m.chat)
       const link = `https://chat.whatsapp.com/${code}`
-      const teks = `﹒⌗﹒🌿 .ৎ˚₊‧  Aquí tienes el link del grupo:\n\n𐚁 ֹ ִ \`GROUP LINK\` ! ୧ ֹ ִ🔗\n☘️ \`Solicitado por :\` @${m.sender.split('@')[0]}\n\n🌱 \`Enlace :\` ${link}`
+      const teks = `✨=== Grupo Oficial ===✨
+
+👤 Solicitado por: @${m.sender.split('@')[0]}
+
+🔗 Enlace: ${link}
+
+🌟 ¡Únete y participa! 🌟`;
       await client.reply(m.chat, teks, m, { mentions: [m.sender] })
     } catch (e) {
       await m.reply(`> An unexpected error occurred while executing command *${usedPrefix + command}*. Please try again or contact support if the issue persists.\n> [Error: *${e.message}*]`)

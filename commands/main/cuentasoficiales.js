1e73f52311c71a7381b52d0695cddcb44e804706
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-03 11:26:11 -0300

    Actualizar cuentasoficiales.js

--- commands/main/cuentasoficiales.js
+++ commands/main/cuentasoficiales.js
@@ -2,32 +2,32 @@ export default {
   command: ['cuentasoficiales'],
   exp: 35,
 
-  run: async (m) => {
+  run: async (m, { sock }) => {
 
     const media = 'https://files.catbox.moe/lcn1kw.mp4'
 
-    let text = `🌑⚔️ BIENVENIDO(A) A LAS CUENTAS OFICIALES ⚔️🌑
+    let text = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 ⚔️🌑
+💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
 
-💖 Shadow-BOT-MD
-El poder oculto se revela solo en las sombras...
-
-CANAL YOSUE
+✅ CANAL OFICIAL - YOSUE
 https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O
 
-CANAL ADO
+✅ CANAL OFICIAL - ADO
 https://whatsapp.com/channel/0029VbBIgz1HrDZg92ISUl2M
 
-COMUNIDAD
+✅ COMUNIDAD OFICIAL
 https://chat.whatsapp.com/ETHW7aP7kOICrR2RBrfE6N
 
-👑 Yosue
-⚔️ Ado`
+👑 Creador Principal: Yosue
+⚔️ Segundo Creador: Ado
 
-    await m.send({
+🌌 Shadow-BOT-MD`
+
+    await sock.sendMessage(m.chat, {
       video: { url: media },
       caption: text,
       gifPlayback: true
-    })
+    }, { quoted: m })
 
   }
 }

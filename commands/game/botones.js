039066dcb13302a6bd423612199ef488fbfad7c7
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-03 10:09:51 -0300

    Actualizar botones.js

--- commands/game/botones.js
+++ commands/game/botones.js
@@ -1,39 +1,13 @@
 export default {
   command: ["botones"],
-  run: async (m, ctx) => {
+  run: async (m) => {
 
-    const conn = ctx.conn || ctx.sock || ctx.client
-    if (!conn || typeof conn.sendMessage !== "function") {
-      console.log("No hay conexión válida en ctx:", Object.keys(ctx || {}))
-      return
-    }
+    await m.reply(`
+Hola soy *Nanno Bot*
 
-    const jid = m.chat || m.key?.remoteJid
-    if (!jid) {
-      console.log("No se pudo obtener el JID del mensaje")
-      return
-    }
-
-    await conn.sendMessage(jid, {
-      text: "Hola soy *Nanno Bot*",
-      footer: "Nanno Bot",
-      templateButtons: [
-        {
-          index: 1,
-          quickReplyButton: {
-            displayText: "📜 Menú",
-            id: ".menu"
-          }
-        },
-        {
-          index: 2,
-          quickReplyButton: {
-            displayText: "🏓 Ping",
-            id: ".ping"
-          }
-        }
-      ]
-    }, { quoted: m })
+📜 .menu
+🏓 .ping
+`)
 
   }
 }

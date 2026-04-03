58cfba4ad361d55b297c451dc4aa80bfeb4ff4c5
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-03 11:23:54 -0300

    Actualizar cuentasoficiales.js

--- commands/main/cuentasoficiales.js
+++ commands/main/cuentasoficiales.js
@@ -2,11 +2,11 @@ export default {
   command: ['cuentasoficiales'],
   exp: 35,
 
-  run: async (m, ctx) => {
+  run: async (m) => {
 
     const media = 'https://files.catbox.moe/lcn1kw.mp4'
 
-    let text = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 ⚔️🌑
+    let text = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙇𝙀𝙎 ⚔️🌑
 💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
 ┈┈┈┈┈┈┈┈┈┈
 💖 𝙎𝙝𝙖𝙙𝙤𝙬-𝘽𝙊𝙏-𝙈𝘿 ⚔️✨
@@ -14,19 +14,19 @@ export default {
 ┈┈┈┈┈┈┈┈┈┈
 ✅ *CANAL OFICIAL - YOSUE*
 https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O
-┈┈┈┈┈┈┈┈┈┈
+
 ✅ *CANAL OFICIAL - ADO*
 https://whatsapp.com/channel/0029VbBIgz1HrDZg92ISUl2M
-┈┈┈┈┈┈┈┈┈┈
+
 ✅ *COMUNIDAD OFICIAL*
 https://chat.whatsapp.com/ETHW7aP7kOICrR2RBrfE6N
-┈┈┈┈┈┈┈┈┈┈
+
 👑 Creador Principal: +58 424-2773183 (Yosue)
 ⚔️ Segundo Creador: +504 9373-2693 (Ado)
-┈┈┈┈┈┈┈┈┈┈
+
 🌌✨ *Shadow-BOT-MD* — El jardín sombrío nunca duerme ✨🌌`
 
-    await ctx.send({
+    await m.reply({
       video: { url: media },
       caption: text,
       gifPlayback: true

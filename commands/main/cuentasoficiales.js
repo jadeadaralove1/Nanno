e9048e73fb7b418ce1e13ab407ae3c94cf7437f3
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-03 11:31:35 -0300

    Actualizar cuentasoficiales.js

--- commands/main/cuentasoficiales.js
+++ commands/main/cuentasoficiales.js
@@ -3,10 +3,10 @@ export default {
   exp: 35,
 
   run: async (m, { conn }) => {
+    try {
+      const media = 'https://files.catbox.moe/lcn1kw.mp4'
 
-    const media = 'https://files.catbox.moe/lcn1kw.mp4'
-
-    let text = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 ⚔️🌑
+      const text = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 ⚔️🌑
 💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
 
 ✅ CANAL OFICIAL - YOSUE
@@ -23,11 +23,15 @@ https://chat.whatsapp.com/ETHW7aP7kOICrR2RBrfE6N
 
 🌌 Shadow-BOT-MD`
 
-    await conn.sendMessage(m.chat, {
-      video: { url: media },
-      caption: text,
-      gifPlayback: true
-    }, { quoted: m })
+      await conn.sendMessage(m.chat, {
+        video: { url: media }, // video o GIF
+        caption: text,
+        gifPlayback: true
+      }, { quoted: m })
 
+    } catch (error) {
+      console.error('Error en comando cuentasoficiales:', error)
+      await conn.sendMessage(m.chat, { text: '❌ Ocurrió un error al ejecutar el comando.' }, { quoted: m })
+    }
   }
 }

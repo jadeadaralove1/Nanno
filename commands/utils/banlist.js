e71c276ac04db82db240e1ffe0c538ce5c798738
jadeadaralove1 <helpsiris1@gmail.com>
2026-04-05 12:07:31 -0300

    Actualizar banlist.js

--- commands/utils/banlist.js
+++ commands/utils/banlist.js
@@ -4,7 +4,7 @@ export default {
   help: ["banlist"],
   rowner: true,
 
-  run: async (m, { conn, isOwner }) => {
+  run: async (m) => {
 
     const chats = Object.entries(global.db.data.chats || {}).filter(v => v[1].isBanned)
     const users = Object.entries(global.db.data.users || {}).filter(v => v[1].banned)
@@ -15,23 +15,20 @@ export default {
    𝐋𝐈𝐒𝐓𝐀 𝐃𝐄 𝐁𝐀𝐍𝐄𝐀𝐃𝐎𝐒
 ╚══✦🌌🎄✦══╝
 
-👤 *Almas selladas por las sombras*:
-├ Total : ${users.length}${users.length ? '\n' + users.map(([jid]) =>
-`├ ${isOwner ? '@' + jid.split('@')[0] : jid}`).join('\n') : '\n├ Ninguno'}
+👤 *Almas selladas por las sombras*
+├ Total : ${users.length}
+${users.length ? users.map(([jid]) => `├ @${jid.split('@')[0]}`).join('\n') : '├ Ninguno'}
 └────
 
-💬 *Dominios prohibidos bajo la nieve*:
-├ Total : ${chats.length}${chats.length ? '\n' + chats.map(([jid]) =>
-`├ ${jid}`).join('\n') : '\n├ Ninguno'}
+💬 *Dominios prohibidos bajo la nieve*
+├ Total : ${chats.length}
+${chats.length ? chats.map(([jid]) => `├ ${jid}`).join('\n') : '├ Ninguno'}
 └────
 
-✨ En esta navidad sombría, el Shadow Garden vigila en silencio...
+✨ El Shadow Garden observa en silencio...
 `.trim()
 
-    await conn.sendMessage(m.chat, {
-      text: caption,
-      mentions: conn.parseMention(caption)
-    }, { quoted: m })
+    await m.reply(caption)
 
   }
 }
